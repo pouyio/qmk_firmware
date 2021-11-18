179d9005e94e15329fc53dd6b87789529c74e632
vicente <pouyio@gmail.com>
2021-11-18 20:41:13 +0100

    updated tap dance values

--- keyboards/sofle/keymaps/pouyio-test/keymap.c
+++ keyboards/sofle/keymaps/pouyio-test/keymap.c
@@ -27,13 +27,16 @@ enum custom_keycodes {
 // PRV_WPC: previous workspace, combo for ctrl+super+j
 // NXT_WPC: next workspace, combo for ctrl+super+k
 
-#define TD_SFT_CAP 0 // unable to add it to the enum because index was too big
+enum tap_dances {
+    SFT_CAP,
+};
 
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_SFT_CAP] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS)
+    [SFT_CAP] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS) // shift on tap, caps lock on double tap
 };
 
+#define TD_SFT_CAP TD(SFT_CAP)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
@@ -54,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT( \
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
-  TD(TD_SFT_CAP),   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
+  TD_SFT_CAP,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
   KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_MPLY,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
                  XXXXXXX,KC_LGUI,KC_LALT, KC_LOWER, KC_SPC,      KC_ENT,  KC_RAISE, KC_DELETE,    XXXXXXX, KC_RALT \
 ),
