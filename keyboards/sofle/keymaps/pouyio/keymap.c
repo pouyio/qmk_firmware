86f4e40bc9d56dd51487c253c9f22fa650a8bd13
pouyio <pouyio@gmail.com>
2022-05-07 18:11:43 +0200

    update to upstream

--- keyboards/sofle/keymaps/pouyio/keymap.c
+++ keyboards/sofle/keymaps/pouyio/keymap.c
@@ -191,7 +191,7 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return OLED_ROTATION_180;
 }
 
-void oled_task_user(void) {
+bool oled_task_user(void) {
     if (is_keyboard_master()) {
         print_status_narrow();
     } else {
